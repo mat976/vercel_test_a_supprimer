814274c933902aafa645dbe0e5d4c48802015405
matisse <matisse.demontis@gmail.com>
2026-05-12 13:44:10 +0400

    DiscussLike - application de discussion

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+      Exemple
+    </h1>
+  );
+}
